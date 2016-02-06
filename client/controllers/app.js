31fd2ea49db1990f20e3fbecb8aa5d7614d3acc4
Siva Krishnan <sivakrishnan@live.com>
2016-02-06 23:56:44 +0530

    inbox style scratch changes

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -10,3 +10,7 @@ Template.home.rendered = function () {
 	$('.button-collapse').sideNav();
     $('.parallax').parallax();
 };
+
+Template.home.list = function() {
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+};
